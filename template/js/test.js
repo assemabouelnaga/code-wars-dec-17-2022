5d53565b12aad4fa79718a941bd11bcac322e034
Assem Abouelnaga <abouelnagaassem@gmail.com>
2022-12-18 21:40:41 +0200

    5 daily coding challenges

--- template/js/test.js
+++ template/js/test.js
@@ -1,59 +1,57 @@
-//Daily code wars Dec 17
+//Daily codewars dec 18
 
-
-// How good are you really
-
-function betterThanAvg(arr,score){
-    let avg = arr.reduce((acc,c) => acc + c) / arr.length;
-    return score > avg
-}
-let newArr = [2,4,6,8,10];
-console.log(betterThanAvg(newArr,5)) 
-
-
-
-//Reverse sequence
-
-function reverseSequence(num){
-    let arr = [];
-    for(let i=1; i <= num ; i++){
-        arr.unshift(i)
-    };
-    return arr
-};
-
-console.log(reverseSequence(6)) 
-
-
-
-//Beginner - reduce but grow
-
-function multiplyNumsInArray(arr){
-    let sum = 1;
-    arr.forEach(e => {
-        sum *= e;
-    });
-    return sum;
+//Count by X
+function multiplesOfANumber(x,n){
+    let newArr = [];
+    newArr.length = n;
+    return newArr.fill(x).map((e,i) => e = e * (i+1));
+    
 }
 
-//can also use reduce:
+console.log(multiplesOfANumber(7,10))
+// can also solve via: 
 
-function multiplyTheNumsInArray(arr){
-    return arr.reduce((acc,c) => a*c, 1)
+const multiplesOfNumber = (x,n) => Array.from({length: n}, (e, i) => x * (i+1));
+
+//If you can't sleep, just count sheep!!
+
+function countSheep(num){
+    let str = '';
+    for(let i = 1; i<=num; i++){
+        str = str + `${i} sheep...`
+    }
+    return str;
+}
+console.log(countSheep(0));
+
+//Transportation on vacation
+
+function rentalAmount(days){
+    let rent;
+    return days >= 7 ? rent = (40 * days) - 50 : days >= 3 ? rent = (40 * days) - 20 : rent = (40 * days);
 }
 
+console.log(rentalAmount(7))
 
+//Grasshopper - Personalized Message
 
-//sentence smash
+function personalizedMessage(name, owner){
+    if(name == owner){
+        return 'Hello boss'
+    }else{
+        return 'Hello guest'
+    }
+}
 
-let sentenceSmash= (arr) => arr.join(' ').trim();
+// Rock Paper Scissors
 
-console.log(sentenceSmash(['hello','world','and','goodbye','world']))
-
-
-
-//convert a string to an array
-
-let stringToArray = (str) => str.split(' ');
-
-console.log(stringToArray("my name is not string"))
+function rockPaperScissors(input1,input2){
+    if(input1 == input2){
+        return "Draw!"
+    }else if(input1 == 'rock' && input2 == 'scissors' || input1 == 'paper' && input2 == 'rock' || input1 == 'scissors' && input2 == 'paper'){
+        return "Player 1 won!"
+    }else {
+        return "Player 2 won!"
+    }
+}
+// not best practice especially as should not let a single line run on for so long
