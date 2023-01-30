a064b52ffe4c491cf4c3156d5e109475ffb87a03
Assem Abouelnaga <abouelnagaassem@gmail.com>
2023-01-30 17:33:58 +0200

    daily codewar

--- template/js/test.js
+++ template/js/test.js
@@ -1913,3 +1913,15 @@ function openOrSenior(data){
 /*---------------------------
 Daily codewars Jan 30
 ---------------------------*/
+
+//Odd or Even?
+
+function oddOrEven(array) {
+    return array.reduce((a,b) => a + b, 0) % 2 == 0 ? 'even' : 'odd'
+}
+
+
+
+/*---------------------------
+Daily codewars Jan 31
+---------------------------*/
